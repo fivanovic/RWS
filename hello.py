424156d4755bbeb97b94befa62ef5c69e180df5d
fivanovic <fivanovic1999@gmail.com>
2021-09-06 13:09:47 +0200

    date and time

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Hello world'
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+@app.route("/")
+def hello():
+   now = datetime.datetime.now()
+   timeString = now.strftime("%Y-%m-%d %H:%M")
+   templateData = {
+      'title' : 'HELLO!',
+      'time': timeString
+      }
+   return render_template('index.html', **templateData)
+if __name__ == "__main__":
+   app.run(host='0.0.0.0', port=80, debug=True)
